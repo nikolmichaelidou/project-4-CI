92d56ec19e1d2a0f7a2a3edf9ced5c238574a940
nikolmichaelidou <nikolmichaelidou.nm@gmail.com>
2022-06-16 15:37:55 +0000

    Added Maps to the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // Ripple animation for buttons
-
 const rippleButton = document.querySelectorAll("button");
 
 function mousePositionToCustomProp(event, element) {
@@ -21,3 +20,43 @@ rippleButton.addEventListener("click", (e) => {
         { once: true }
     );
 });
+
+// Google Maps //
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6,
+        center: { lat: 51.92824554956347, lng: 4.524163786173565 }
+    });
+    const infoWindow = new google.maps.InfoWindow({
+        content: "",
+        disableAutoPan: true,
+    });
+    // An array of alphabetical characters used to label the markers.
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Add some markers to the map.
+    const markers = locations.map((position, i) => {
+        const label = labels[i % labels.length];
+        const marker = new google.maps.Marker({
+            position,
+            label,
+        });
+
+        marker.addListener("click", () => {
+            infoWindow.setContent(label);
+            infoWindow.open(map, marker);
+        });
+        return marker;
+    });
+
+    // Add a marker clusterer to manage the markers.
+    new mazrkerClusterer({ map, markers });
+}
+
+const locations = [
+    { lat: -33.718234, lng: 150.363181 },
+    { lat: -33.727111, lng: 150.371124 },
+    { lat: 52.12119841758262, lng: 4.279310900297896 }
+];
+
+window.initMap = initMap;
+const markerCluster = new markerClusterer.MarkerClusterer({ map, markers });
