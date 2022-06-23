63a8eec3e20a4aa1070ddfac2196e2e267e48a57
nikolmichaelidou <nikolmichaelidou.nm@gmail.com>
2022-06-23 11:36:08 +0000

    Added another <div> and started the README.md

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,44 @@
-// Ripple animation for buttons
-const rippleButton = document.querySelectorAll("button");
-
-function mousePositionToCustomProp(event, element) {
-    let posX = event.offsetX;
-    let posY = event.offsetY;
-
-    element.style.setProperty("--x", posX + "px");
-    element.style.setProperty("--y", posY + "px");
-}
-
-rippleButton.addEventListener("click", (e) => {
-    mousePositionToCustomProp(e, rippleButton);
-    rippleButton.classList.add("pulse");
-    rippleButton.addEventListener(
-        "animationend",
-        () => {
-            rippleButton.classList.remove("pulse");
-        },
-        { once: true }
-    );
-});
 
 // Google Maps //
-function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
+function initMap ()
+{
+    const map = new google.maps.Map( document.getElementById( "map" ), {
         zoom: 6,
         center: { lat: 51.92824554956347, lng: 4.524163786173565 }
-    });
-    const infoWindow = new google.maps.InfoWindow({
+    } );
+    const infoWindow = new google.maps.InfoWindow( {
         content: "",
         disableAutoPan: true,
-    });
+    } );
     // An array of alphabetical characters used to label the markers.
     const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     // Add some markers to the map.
-    const markers = locations.map((position, i) => {
-        const label = labels[i % labels.length];
-        const marker = new google.maps.Marker({
+    const markers = locations.map( ( position, i ) =>
+    {
+        const label = labels[ i % labels.length ];
+        const marker = new google.maps.Marker( {
             position,
             label,
-        });
+        } );
 
-        marker.addListener("click", () => {
-            infoWindow.setContent(label);
-            infoWindow.open(map, marker);
-        });
+        marker.addListener( "click", () =>
+        {
+            infoWindow.setContent( label );
+            infoWindow.open( map, marker );
+        } );
         return marker;
-    });
+    } );
 
     // Add a marker clusterer to manage the markers.
-    new mazrkerClusterer({ map, markers });
+    new markerClusterer( { map, markers } );
 }
 
 const locations = [
     { lat: -33.718234, lng: 150.363181 },
     { lat: -33.727111, lng: 150.371124 },
-    { lat: 52.12119841758262, lng: 4.279310900297896 }
+    { lat: 52.12119841758262, lng: 4.279310900297896 },
+    { lat: 52.07799717535008, lng: 4.294741738657255 },
 ];
 
 window.initMap = initMap;
-const markerCluster = new markerClusterer.MarkerClusterer({ map, markers });
+const markerCluster = new markerClusterer.MarkerClusterer( { map, markers } );
